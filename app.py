b344fb24aaf687b56cac2087da8fe7d6d068c1d4
Eric <eroija@gmail.com>
2024-10-12 09:24:41 +0300

    Create templates folder to organize HTML files

---
+++ app.py
@@ -0,0 +1,8 @@
+from flask import Flask, render_template, request, jsonify
+from flask_mysqldb import MySQL
+from pprint import pprint
+import
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
