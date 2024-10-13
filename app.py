599a3463c7023daad9ee37dfb5060111c8f8725d
Eric <eroija@gmail.com>
2024-10-13 08:27:31 +0300

    Fix issues with clan_member.html form handling and field validation

--- app.py
+++ app.py
@@ -48,14 +48,16 @@ def clan_member():
     if request.method == 'POST':
         # Get family details from the form
         family_id = request.form.get('family_id')
-        family = request.form.get('family')
         name = request.form.get('name')
+        occupation = request.form.get('occupation')
+        phone_number = request.form.get('phone')
+        location = request.form.get('location')
 
         # Create a cursor object
         cur = mysql.connection.cursor()
 
         # Execute the SQL query
-        cur.execute("INSERT INTO imamai_family(family_id, family, name) VALUES (%s, %s, %s)", (family_id, family, name))
+        cur.execute("INSERT INTO clan_member(family_id, name, occupation, phone_number, location) VALUES (%s, %s, %s, %s, %s)", (family_id, name, occupation, phone_number, location))
 
         # Commit to the database
         mysql.connection.commit()
@@ -64,7 +66,7 @@ def clan_member():
         cur.close()
 
         # Return a success message
-        return 'family added successfully!'
+        return 'clan member added successfully!'
     else:
         # If it's not a POST request, you can display a form or a message
         return render_template('clan_member.html')
